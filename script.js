50bb414516a46721ae0a38d775351f9d6646c7d4
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-02 16:26:12 -0600

    pretty checker board

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ document.body.style.height = "100%";
 for (var i = 0; i < 9; i++) {
     
   for (var j = 0; j < 9; j++) {
-    
+    var random1 = Math.floor(Math.random() * 256)
+    var random2 = Math.floor(Math.random() * 256)
+    var random3 = Math.floor(Math.random() * 256)
     var checker = document.createElement("div");
     checker.style.height = "11.1%";
     checker.style.width = "11.1%";
@@ -13,15 +15,15 @@ for (var i = 0; i < 9; i++) {
     checker.style.paddingBottom = "11.1%";
     if (i % 2 === 0) {
       if (j % 2 === 0) {
-        checker.style.backgroundColor = 'black';
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
       } else {
-        checker.style.backgroundColor = "red";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
       }
     } else {
       if (j % 2 === 0) {
-        checker.style.backgroundColor = "red";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
       } else {
-        checker.style.backgroundColor = "black";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
       }
     }
     document.body.appendChild(checker);
