d8beab2e65f21e1f7c543bbbad3ff9aef288276d
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-02 17:52:48 -0600

    Its colorful and gradient

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var delay = 1000;
 document.body.style.width = "100%";
 document.body.style.height = "100%";
+document.body.style.backgroundColor = "blue";
+var a = 0.0;
 
 for (var i = 0; i < 9; i++) {
     
@@ -15,15 +17,19 @@ for (var i = 0; i < 9; i++) {
     checker.style.paddingBottom = "11.1%";
     if (i % 2 === 0) {
       if (j % 2 === 0) {
-        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + "," + a + ")";
+        a += .015;
       } else {
-        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + "," + a + ")";
+        a += .015;
       }
     } else {
       if (j % 2 === 0) {
-        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + "," + a + ")";
+        a += .015;
       } else {
-        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + ")";
+        checker.style.backgroundColor = "rgb("+ random1 +","+random2+"," + random3 + "," + a + ")";
+        a += .015;
       }
     }
     document.body.appendChild(checker);
