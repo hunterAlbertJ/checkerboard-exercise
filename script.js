8fe4fd30b634997ce1161a614773491c6ca38aa1
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-02 16:18:03 -0600

    regular checker board

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Your JS goes here
+var delay = 1000;
+document.body.style.width = "100%";
+document.body.style.height = "100%";
+
+for (var i = 0; i < 9; i++) {
+    
+  for (var j = 0; j < 9; j++) {
+    
+    var checker = document.createElement("div");
+    checker.style.height = "11.1%";
+    checker.style.width = "11.1%";
+    checker.style.float = "left";
+    checker.style.paddingBottom = "11.1%";
+    if (i % 2 === 0) {
+      if (j % 2 === 0) {
+        checker.style.backgroundColor = 'black';
+      } else {
+        checker.style.backgroundColor = "red";
+      }
+    } else {
+      if (j % 2 === 0) {
+        checker.style.backgroundColor = "red";
+      } else {
+        checker.style.backgroundColor = "black";
+      }
+    }
+    document.body.appendChild(checker);
+  }
+}
+
